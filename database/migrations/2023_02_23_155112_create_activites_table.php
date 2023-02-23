fe28347b182fde8e876f35d3105e07408286b515
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2023-02-23 17:47:05 +0100

    update

---
+++ database/migrations/2023_02_23_155112_create_activites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activites', function (Blueprint $table) {
+            $table->increment('id');
+            $table->string('libelle');
+            $table->text('description', 2550);
+            $table->string('imgUrl');
+            $table->year('annee');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activites');
+    }
+};
---
+++ database/migrations/2023_02_23_155134_create_evenements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evenements', function (Blueprint $table) {
+            $table->increment('id');
+            $table->string('libelle');
+            $table->text('description', 2550);
+            $table->string('imgUrl');
+            $table->string('videoUrl');
+            $table->year('annee');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evenements');
+    }
+};
